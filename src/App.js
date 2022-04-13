54177a38f47172b318fe1c4d9ffb6bdcba90792f
Gagan <gaganichake@yahoo.com>
2022-04-13 13:39:56 -0500

    useCallback() and its Dependencies

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('App component re-evaluated!');
 
   const toggleParagraphHandler = useCallback(() => {
+    if(allowToggle) {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
